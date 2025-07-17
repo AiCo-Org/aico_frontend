c142a53a38025a5fdf8acc8a5782288c816937b3
Jadest13 <jadest13x03@gmail.com>
2025-07-18 06:36:28 +0900

    #1 [feat] 임시 푸터 추가

--- src/pages/Index/page.tsx
+++ src/pages/Index/page.tsx
@@ -19,6 +19,10 @@ import Reviewer6PNG from '@assets/landing/reviewer/reviewer_6.png';
 import IndexVido from '@assets/landing/video.mp4';
 import styles from './page.module.css';
 
+const handleClickPreRegistration = () => {
+  window.open('https://forms.gle/L62vmyWkw22iodBa6', '_blank');
+};
+
 const IndexPageComponent1 = () => {
   return (
     <div className={styles.heroSection}>
@@ -33,7 +37,7 @@ const IndexPageComponent1 = () => {
           <p className={styles.heroTitle2}>With Ai-Co</p>
         </div>
         <p className={styles.heroDesc}>Just type in one line - the best AI team handles the rest.</p>
-        <button className={styles.heroButton}>
+        <button className={styles.heroButton} onClick={handleClickPreRegistration}>
           Sign Up for Pre-Registration
           <ChevronRightSVG className={styles.heroButtonIcon} />
         </button>
@@ -166,23 +170,34 @@ const IndexPageComponent6 = () => {
         <p className={styles.section6Title1}>Be the first to join the huge wave that creates the future.</p>
         <p className={styles.section6Title2}>Change the world with Ai-Co</p>
       </div>
-      <button className={styles.section6Button}>
+      <button className={styles.section6Button} onClick={handleClickPreRegistration}>
         Sign Up for Pre-Registration
         <ChevronRightSVG className={styles.section6ButtonIcon} />
       </button>
+      <p className={styles.section6ButtonDesc}>
+        If you pre-register, you can use it first
+        <br />
+        after the product is released
+      </p>
     </div>
   );
 };
 
 const IndexPage = () => {
   return (
-    <div className={styles.indexPageWrap}>
-      <IndexPageComponent1 />
-      <IndexPageComponent2 />
-      <IndexPageComponent4 />
-      <IndexPageComponent3 />
-      <IndexPageComponent5 />
-      <IndexPageComponent6 />
+    <div className={styles.indexPage}>
+      <div className={styles.indexPageWrap}>
+        <IndexPageComponent1 />
+        <IndexPageComponent2 />
+        <IndexPageComponent4 />
+        <IndexPageComponent3 />
+        <IndexPageComponent5 />
+        <IndexPageComponent6 />
+      </div>
+      <div className={styles.section7}>
+        <p className={styles.section7Text}>copyright 2025 Ai-Co. All rights reserved.</p>
+        <p className={styles.section7Text}>contact us: apsxhtm0625@gmail.com</p>
+      </div>
     </div>
   );
 };
